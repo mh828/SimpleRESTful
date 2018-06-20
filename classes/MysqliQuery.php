6c5fca7988a1ce08f20b8dc20ac6eb93830802bc
mh <mh.hasnapour@gmail.com>
2018-06-20 09:17:36 +0430

    MysqliQuery hellper

---
+++ classes/MysqliQuery.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mh
+ * Date: 06/20/2018
+ * Time: 08:54
+ */
+
+class MysqliQuery
+{
+    public function insert($tableName, $columnsAndValueArray)
+    {
+        $cols = $vals = '';
+
+        foreach ($columnsAndValueArray as $k => $v) {
+            $cols .= " `{$k}`,";
+            if ($v == '' || $k == null) {
+                $vals .= ' NULL,';
+            } else {
+                $vals .= " '{$v}',";
+            }
+        }
+
+        $cols = rtrim($cols, ',');
+        $vals = rtrim($vals, ',');
+
+        return "INSERT INTO `{$tableName}` ({$cols}) VALUES ({$vals})";
+    }
+
+    public function update($tableName, $columnsAndValueArray, $condition)
+    {
+        $temp = '';
+        foreach ($columnsAndValueArray as $k => $v) {
+            $val = ' NULL';
+            if ($v != '' && $k != null) {
+                $val = " '{$v}'";
+            }
+
+            $temp .= " `{$k}` = {$val},";
+        }
+
+        $temp = rtrim($temp, ',');
+
+        return "UPDATE  `{$tableName}`  SET {$temp} WHERE $condition ";
+    }
+}
