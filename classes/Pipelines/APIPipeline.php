f1fd4c936130938663b88fa1f48b6a36efa942a2
Mahdi Hasanpour <mh.hasnapour@gmail.com>
2019-12-26 14:23:43 +0330

    Pipelining process http management php 7.1 minimum version required

---
+++ classes/Pipelines/APIPipeline.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Pipelines;
+
+
+use Helper\HTTPRequestHelper;
+use Interfaces\IPipeline;
+
+class APIPipeline implements IPipeline
+{
+
+    public function handle(\PipeLineCore $core, \Closure $next)
+    {
+
+    }
+}
