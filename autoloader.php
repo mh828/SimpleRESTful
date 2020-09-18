09be136fc4c3b1df464f1462ca0401ae24793ac1
mahdi <2282698+mh828@users.noreply.github.com>
2020-09-18 13:11:36 +0430

    fixt autoloader

--- autoloader.php
+++ autoloader.php
@@ -1,5 +1,5 @@
 <?php
 spl_autoload_register(function ($class) {
     $path = str_replace('\\', '/', $class);
-    @include dirname(__DIR__) . '/' . $path . '.php';
+    @include __DIR__ . '/' . str_replace('SimpleRESTful', '', $path) . '.php';
 });
